7d1568d921ffe158f8beb6d38290ff91fa9bed40
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-14 20:13:50 +0200

    Happy B-Days! Додано: 1. Навів красу в schedule_block.py

--- happy_bot/core/bot_scheduler/schedule_block.py
+++ happy_bot/core/bot_scheduler/schedule_block.py
@@ -1,28 +1,11 @@
-from happy_bday.settings import TIME_ZONE
-from happy_bot.bd_bot import bot
-# ПЛАНУВАЛЬНИК
+"""ПЛАНУВАЛЬНИК"""
+
+# Планувальник
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from happy_bot.core.handlers import schedul_task
-from datetime import datetime, timedelta
+
+# Імпорт з пакета налаштувань
+from happy_bday.settings import TIME_ZONE
 
 
-"""Блок планувальника"""
 scheduler = AsyncIOScheduler(timezone=TIME_ZONE)
 reminders_scheduler = AsyncIOScheduler(timezone=TIME_ZONE)
-
-scheduler.add_job(schedul_task.send_message_time,
-                  trigger='date', run_date=datetime.now() + timedelta(seconds=10),
-                  kwargs={'bot': bot})
-
-scheduler.add_job(schedul_task.send_message_interval,
-                  trigger='interval', minutes=10, kwargs={'bot': bot})
-
-scheduler.add_job(schedul_task.send_message_date,
-                  trigger='date', run_date=schedul_task.job_date,
-                  kwargs={'bot': bot})
-
-
-
-
-"""Кінець блока планувальника"""
-
--- happy_bot/core/handlers/schedul_task.py
+++ happy_bot/core/handlers/schedul_task.py
@@ -1,36 +1,8 @@
 from aiogram import Bot
 from aiogram.types import Message
 
-from happy_bday.settings import ADMIN_ID
-from datetime import datetime
-
 from happy_bot.bd_bot import bot
 from happy_bot.core.handlers.send_media import get_picture
-
-now = datetime.now()
-
-# Задаємо дату, коли потрібно викликати функцію
-job_date = datetime(2023, 3, 6, 11, 44, 0)
-
-
-async def send_message_time(bot: Bot):
-    await bot.send_message(
-        ADMIN_ID, 'Це повідомлення отримано через 5 сек після запуску бота.'
-    )
-
-
-async def send_message_cron(bot: Bot):
-    await bot.send_message(ADMIN_ID, f'Це повідомлення відправляється '
-                                     f'кожен день у визначений час o {now}.')
-
-
-async def send_message_interval(bot: Bot):
-    await bot.send_message(ADMIN_ID, 'Це повідомлення відправляється з інтервалом в 10 хв.')
-
-
-async def send_message_date(bot: Bot):
-    await bot.send_message(ADMIN_ID, f'Це повідомлення отримано по заданій даті:'
-                                     f'{job_date}.')
 
 
 async def send_message_glory(bot: Bot, chat_id: int):
@@ -39,4 +11,3 @@ async def send_message_glory(bot: Bot, chat_id: int):
 
 async def send_message_chat_gpt(message: Message):
     await get_picture(message.from_user.id, bot, 'Я, звісно, не ChatGPT, але спробую допомогти)', 'chat')
-    # await message.answer('\U0001F916 \n\nЯ, звісно, не ChatGPT, але спробую допомогти)')

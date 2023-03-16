c0226488ae179785748f77d002641a5e58f513c0
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-16 20:07:49 +0200

    Happy B-Days! Додано: 1. Рефакторинг admin.py в happy_bot

--- happy_bot/admin.py
+++ happy_bot/admin.py
@@ -1,3 +1,13 @@
+"""РЕЄСТРАЦІЯ МОДЕЛІ PROFILE"""
 from django.contrib import admin
+from happy_bot.models import Profile
 
-# Register your models here.
+
+# Register the Profile model
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'telegram_chat_id')  # Перелік полів,
+    # які ми хочемо бачити в адмін-панелі
+    list_display_links = ('user', 'telegram_chat_id')  # Поля за якими можна перейти на відповідну статтю
+    search_fields = ('user',)  # За якими полями можна буде проводити пошук
+    list_filter = ('user',)  # Призначаємо поля для фільтрації списку статей
--- happy_bot/models.py
+++ happy_bot/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     telegram_chat_id = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Профіль'
+        verbose_name_plural = 'Профілі'

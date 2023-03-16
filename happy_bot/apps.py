a2274029da9913b92406a82f58407e9a8f057480
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-16 20:13:44 +0200

    Happy B-Days! Додано: 1. Рефакторинг apps.py в happy_bot

--- happy_bot/apps.py
+++ happy_bot/apps.py
@@ -1,10 +1,20 @@
+"""РЕЄСТРАЦІЯ ДОДАТКУ HAPPY BOT"""
 from django.apps import AppConfig
 
 
 class HappyBotConfig(AppConfig):
+    """
+    Class representing a Django application HappyBot and its configuration.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'happy_bot'
 
-    def ready(self):
+    def ready(self) -> None:
+        """
+        This method code runs when Django starts.
+
+        :param self: Represent the instance of the class
+        :return: None
+        """
         import happy_bot.signals
 

5b1f58dbd3b009293ae66b1a78d71c5375a92072
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-16 19:34:53 +0200

    Happy B-Days! Додано: 1. Рефакторинг search_state.py

--- happy_bot/core/states/search_state.py
+++ happy_bot/core/states/search_state.py
@@ -1,5 +1,9 @@
+"""ЗАГАЛЬНИЙ STATE ДЛЯ ПОШУКУ"""
 from aiogram.fsm.state import State, StatesGroup
 
 
 class SearchState(StatesGroup):
+    """
+    State object for searching
+    """
     waiting_for_object = State()

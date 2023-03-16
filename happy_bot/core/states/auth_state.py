58f39893b8e54ac460a7d43e27954c08030f0f43
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-16 19:29:21 +0200

    Happy B-Days! Додано: 1. Рефакторинг auth_state.py

--- happy_bot/core/states/auth_state.py
+++ happy_bot/core/states/auth_state.py
@@ -1,6 +1,10 @@
+"""STATE ДЛЯ АВТОРИЗАЦІЇ"""
 from aiogram.fsm.state import State, StatesGroup
 
 
 class AuthState(StatesGroup):
+    """
+    State object for authorization
+    """
     waiting_for_username = State()
     waiting_for_password = State()

c606ec178899552af7dfb64771180ee974c99248
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-16 20:39:37 +0200

    Happy B-Days! Додано: 1. Рефакторинг happy_tags.py

--- happy_bot/signals.py
+++ happy_bot/signals.py
@@ -1,14 +1,4 @@
+"""ЗВ'ЯЗОК ЧЕРЕЗ SIGNALS З HAPPY SITE"""
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from happy_site.models import Reminder
-
-
-# @receiver(post_delete, sender=Reminder)
-# async def delete_reminder(sender, instance, **kwargs):
-#     """
-#     Цей код створює функцію delete_reminder, яка нічого не робить.
-#     Ми використовуємо цей код, щоб Django не викидав помилки про те,
-#     що немає слухача сигналу в додатку happy_bot.
-#     """
-#     pass
-
--- happy_site/templatetags/happy_tags.py
+++ happy_site/templatetags/happy_tags.py
@@ -1,15 +1,38 @@
+"""ТЕГ ДЛЯ ВІДОБРАЖЕННЯ НАГАДУВАНЬ"""
+
+# Загальні імпорти
 import pytz
+
+# Імпорти Django
 from django import template
 from django.urls import reverse
 from django.utils.html import format_html
-from ..views import get_reminders_by_birthday
+from django.utils.safestring import SafeString
+
+# Імпорти з пакета налаштувань
 from happy_bday.settings import TIME_ZONE
+
+# Внутрішні імпорти
+from happy_site.models import BDays
+from happy_site.views import get_reminders_by_birthday
+
 
 register = template.Library()
 
 
 @register.simple_tag
-def reminders_for_birthday(birthday):
+def reminders_for_birthday(birthday: BDays) -> SafeString | str:
+    """
+    The reminders_for_birthday function returns a string of HTML code
+    that contains all the reminders for a given birthday.
+    The function takes one argument, which is an instance of the BDays model class.
+    The function uses this argument to get all Reminder objects associated
+    with this particular birthday and then creates an HTML string containing
+    links to edit these reminders and delete them.
+
+    :param birthday: BDays: Get the reminders for a birthday
+    :return: A string with html code
+    """
     if not (reminders := get_reminders_by_birthday(birthday).order_by('date_time')):
         return ''
     html = "<p style='font-weight: bold; color: steelblue; text-align: left;'>Пам'ятки:</p>"

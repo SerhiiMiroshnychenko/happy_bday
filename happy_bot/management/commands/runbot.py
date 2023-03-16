bb51933f0b0c51c8678f64ced3a8298b5b2e7b3e
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-16 19:56:52 +0200

    Happy B-Days! Додано: 1. Рефакторинг named_tuple_classes.py

--- happy_bot/management/commands/runbot.py
+++ happy_bot/management/commands/runbot.py
@@ -1,16 +1,27 @@
-from django.core.management.base import BaseCommand
+"""КОМАНДА MANAGE.PY ДЛЯ ЗАПУСКУ БОТА"""
 
+
+# Базові імпорти
 import asyncio
 import logging
-import platform
 
-from happy_bot.core.bot_dicpatcher.set_dispatcher import dp
+# Імпорти Django
+from django.core.management.base import BaseCommand
+
+# Внутрішні імпорти
 from happy_bot.bd_bot import bot
-
+from happy_bot.core.bot_dicpatcher.set_dispatcher import dp
 from happy_bot.core.bot_scheduler.schedule_block import scheduler, reminders_scheduler
 
 
-async def start():
+async def start() -> None:
+    """
+    The start function is the entry point for the bot.
+    It starts a polling loop, which listens to incoming
+    messages and calls handlers when necessary.
+
+    :return: None; Init the bot object
+    """
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - [%(levelname)s] - %(name)s - '
                                '(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
@@ -26,9 +37,20 @@ async def start():
 
 
 class Command(BaseCommand):
+    """
+    The class for bot's running
+    """
     help = 'RUN COMMAND: python manage.py runbot'
 
-    def handle(self, *args, **options):
-        # if platform.system() == 'Windows':
-        #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    def handle(self, *args, **options) -> None:
+        """
+        The handle function is the entry point for command execution.
+        It should perform any work required to execute the command, and return an exit code.
+        The default implementation calls self.execute(), which simply raises NotImplementedError.
+
+        :param self: Represent the instance of the class
+        :param args: Pass a variable number of arguments to a function
+        :param options: Pass in options from the command line
+        :return: None; Starting the bot.
+        """
         asyncio.run(start())

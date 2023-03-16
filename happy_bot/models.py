98ee30c7f53008ade3f3b7e02ad0f5db2679e6cc
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-16 20:17:18 +0200

    Happy B-Days! Додано: 1. Рефакторинг models.py в happy_bot

--- happy_bot/models.py
+++ happy_bot/models.py
@@ -1,8 +1,12 @@
+"""МОДЕЛЬ PROFILE"""
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    Модель профілю користувача в телеграм боті
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     telegram_chat_id = models.CharField(max_length=50, blank=True, null=True)
 

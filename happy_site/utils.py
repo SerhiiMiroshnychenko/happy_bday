ed10a061e269382bb9c0b29e73bc8f66aa180855
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-17 11:46:28 +0200

    Happy B-Days! Додано: 1. Рефакторинг utils.py додатку happy site

--- happy_site/utils.py
+++ happy_site/utils.py
@@ -1,20 +1,27 @@
+"""MENU and DATA MIXIN"""
 
-
-menu = [{'title': "Про сайт", 'url_name': 'about'},
-        {'title': "Додати статтю", 'url_name': 'add_page'},
-        {'title': "Зворотній зв'язок", 'url_name': 'contact'},
+menu = [{'title': "Дні народження", 'url_name': 'b_days'},
+        {'title': "Додати день народження", 'url_name': 'add_bday'},
         ]
 
 
 class DataMixin:
+    """
+    Клас-розширення для класів-відображень сайту
+    """
+    @staticmethod
+    def get_user_context(**kwargs) -> dict:
+        """
+        The get_user_context function is a helper function that returns the context dictionary for the user.
+        The context dictionary contains all the information needed to render a template, including:
+            - The menu items (menu)
+            - The current page title (title)
+            - Any other variables passed in via kwargs
 
-    def get_user_context(self, **kwargs):
+        :param kwargs: Pass a variable number of keyword arguments to the function
+        :return: A dictionary with the menu key and its value
+        """
         context = kwargs
-
         user_menu = menu.copy()
-        if not self.request.user.is_authenticated:
-            user_menu.pop(1)
-
         context['menu'] = user_menu
-
         return context

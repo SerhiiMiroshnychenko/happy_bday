bc79a1c3549b4501164b9cf2b0d484588da9e891
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-03-17 11:34:16 +0200

    Happy B-Days! Додано: 1. Рефакторинг urls.py додатку happy site

--- happy_site/urls.py
+++ happy_site/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, re_path
-from .views import *
+"""ШЛЯХИ ДО СТОРІНОК САЙТУ"""
 
+from django.urls import path
+from happy_site.views import sing_out, edit_bd, edit_reminder, delete_bd, del_reminder, \
+    RegisterUser, LoginUser, AddBDay, BDayList, ShowBDay, AddReminder, NextBDay
 
 urlpatterns = [
     path('', NextBDay.as_view(), name='home'),  # http://127.0.0.1:8080/
@@ -8,15 +10,11 @@ urlpatterns = [
     path('logout/', sing_out, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('bdays/', BDayList.as_view(), name='b_days'),  # http://127.0.0.1:8080/bdays/
-
     path('bday/<int:bday_pk>/', ShowBDay.as_view(), name='bday'),
-
     path('add_bday/', AddBDay.as_view(), name='add_bday'),  # http://127.0.0.1:8080/add_bday/
     path('bdays/<int:bd_id>/edit', edit_bd, name='edit'),
     path('bdays/<int:bd_id>/delete', delete_bd, name='delete'),
     path('bdays/<int:bd_id>/add_reminder', AddReminder.as_view(), name='add_reminder'),
     path('edit_reminder/<int:reminder_id>', edit_reminder, name='edit_reminder'),
     path('edit_reminder/<int:reminder_id>/delete', del_reminder, name='del_reminder'),
-
 ]
-
